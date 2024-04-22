eaa8480ed319b74b3ddfc4abcd59d17102932b64
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2024-04-22 15:41:10 +0300

    updated

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    const Today=new Date();
+
+  return (
+    <footer>
+            <p>Copyright &copy; {Today.getFullYear()} </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header style={{
+        backgroundColor:'mediumblue',
+        color:'#fff'
+    }}><h1>APPS TO CREATE</h1></header>
+
+    
+  )
+}
+
+export default Header
