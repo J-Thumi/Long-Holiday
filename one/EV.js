428c20d325b12a2e88ee62aea1057d4f18939427
Josphat Thumi <145795204+J-Thumi@users.noreply.github.com>
2024-04-23 13:33:57 +0300

    Add files via upload

---
+++ one/EV.js
@@ -0,0 +1,22 @@
+document.getElementById("plus1").onclick=function(){
+    document.getElementById("plus1").src="icon-minus.svg"
+    document.getElementById(" ")
+
+
+}
+document.getElementById("plus2").onclick=function(){
+    document.getElementById("plus2").src="icon-minus.svg"
+
+}
+document.getElementById("plus3").onclick=function(){
+    document.getElementById("plus3").src="icon-minus.svg"
+
+}
+document.getElementById("plus4").onclick=function(){
+    document.getElementById("plus4").src="icon-minus.svg"
+
+}
+document.getElementById("plus5").onclick=function(){
+    document.getElementById("plus5").src="icon-minus.svg"
+
+}
